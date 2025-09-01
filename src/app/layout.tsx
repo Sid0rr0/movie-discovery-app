d484c4a36bd8bc094f7d3b8e858e1df0ed68803b
Thanh Hung Le <thanh-hung.le@quanti.cz>
2025-09-01 15:30:01 +0200

    chore: add stylistic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} dark`}>
-      <Providers>{children}</Providers>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
